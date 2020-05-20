c6c7ea45f3be09d98530ebc0e6badcf736a92700
Mariska <0983425@hr.nl>
2020-05-20 11:03:54 +0200

    added Game class

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,3 +1,21 @@
+class Game {
+    constructor() {
+        this.player = Player();
+        this.worms = Worms();
+        this.background = Background();
+        this.platform = Platform();
+        this.score = Score();
+        this.gameLoop();
+    }
+    gameLoop() {
+        this.player.update();
+        this.worms.update();
+        this.background.update();
+        this.platform.update();
+        this.score.update();
+        requestAnimationFrame(() => this.gameLoop());
+    }
+}
 class Pineapple {
     constructor() {
         console.log("Hallo!");
