842756ab2af9bcc860d66a3e793c96b7ad528725
Mariska <0983425@hr.nl>
2020-05-26 17:05:16 +0200

    Uncommented lines of code.

--- dev/game.ts
+++ dev/game.ts
@@ -2,33 +2,33 @@ class Game {
     // Global variables
     private player : Player 
     private worm : Worm 
-    // private background : Background
-    // private platform : Platform
-    // private score : Score
+    private background : Background
+    private platform : Platform
+    private score : Score
 
     constructor() {
         // Makes new objects
         this.player = new Player()
         this.worm = new Worm() 
-        // this.background = new Background() 
-        // this.platform = new Platform() 
-        // this.score = new Score()
+        this.background = new Background() 
+        this.platform = new Platform() 
+        this.score = new Score()
 
-        // Update frames
-        // this.gameLoop() 
+        //Update frames
+        this.gameLoop() 
     }
 
-    // gameLoop(){
-    //     // Updates the screen 
-    //     this.player.update()
-    //     this.worm.update()
-    //     this.background.update()
-    //     this.platform.update()
-    //     this.score.update()
+    gameLoop(){
+        // Updates the screen 
+        this.player.update()
+        this.worm.update()
+        this.background.update()
+        this.platform.update()
+        this.score.update()
 
-    //     // Resfreshes the screen
-    //     requestAnimationFrame(() => this.gameLoop())
-    // }
+        // Resfreshes the screen
+        requestAnimationFrame(() => this.gameLoop())
+    }
 
 }
 
