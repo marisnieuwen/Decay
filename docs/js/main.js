cb2d7f30b1b2aba3928205060f03d68ca24b31d4
Robin Michael van Doorn <robinmvdoorn@gmail.com>
2020-06-10 15:19:17 +0200

    Cleanup stylesheet.

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,12 +1,6 @@
 class Game {
     constructor() {
         this.platform = [];
-<<<<<<< HEAD
-        for (let i = 0; i < 1; i++) {
-            this.platform.push(new Platform());
-        }
-        this.player = new Player();
-=======
         this.worm = [];
         for (let i = 0; i < 7; i++) {
             this.platform.push(new Platform());
@@ -15,19 +9,11 @@ class Game {
         for (let i = 0; i < 1; i++) {
             this.worm.push(new Worm(i * 45, -47));
         }
->>>>>>> db80a2449a2bea7753a3f0c518edf4538537daa5
         this.gameloop();
+        requestAnimationFrame(() => this.gameloop());
     }
     gameloop() {
         for (const platform of this.platform) {
-<<<<<<< HEAD
-            platform.update();
-            if (this.checkCollision(platform.getRectangle(), this.player.getRectangle())) {
-                this.player.update2();
-            }
-        }
-        this.player.update();
-=======
             platform.placement();
             if (this.checkCollision(platform.getRectangle(), this.player.getRectangle())) {
                 this.player.stopMove();
@@ -40,7 +26,6 @@ class Game {
             }
         }
         this.player.move();
->>>>>>> db80a2449a2bea7753a3f0c518edf4538537daa5
         requestAnimationFrame(() => this.gameloop());
     }
     checkCollision(a, b) {
@@ -52,101 +37,6 @@ class Game {
 }
 window.addEventListener("load", () => new Game());
 class Platform {
-<<<<<<< HEAD
-    constructor() {
-        this.platform = document.createElement("platform");
-        let game = document.getElementsByTagName("game")[0];
-        game.appendChild(this.platform);
-    }
-    getRectangle() {
-        return this.platform.getBoundingClientRect();
-    }
-    update() {
-        this.platform.style.transform = `translate(${this.x}px, ${this.y}px)`;
-    }
-}
-class Player {
-    constructor() {
-        this.x = 0;
-        this.y = 0;
-        this.jumpy = 0;
-        this.downSpeed = 0;
-        this.upSpeed = 0;
-        this.rightSpeed = 0;
-        this.leftSpeed = 0;
-        this.jumpHeight = -10;
-        this.jumping = false;
-        this.gravity = 0.2;
-        this.player = document.createElement("player");
-        let game = document.getElementsByTagName("game")[0];
-        game.appendChild(this.player);
-        this.upkey = 38;
-        this.downkey = 40;
-        this.rightkey = 39;
-        this.leftkey = 37;
-        this.x = 370;
-        this.y = 382;
-        window.addEventListener("keydown", (e) => this.onKeyDown(e));
-        window.addEventListener("keyup", (e) => this.onKeyUp(e));
-    }
-    getRectangle() {
-        return this.player.getBoundingClientRect();
-    }
-    onKeyDown(e) {
-        switch (e.keyCode) {
-            case this.upkey:
-                if (!this.jumping) {
-                    this.jumping = true;
-                    console.log("jumped");
-                    this.jumpy = this.jumpHeight;
-                }
-            case this.downkey:
-                this.downSpeed = 3;
-                break;
-            case this.rightkey:
-                this.rightSpeed = 3;
-                break;
-            case this.leftkey:
-                this.leftSpeed = 3;
-                break;
-        }
-    }
-    onKeyUp(e) {
-        switch (e.keyCode) {
-            case this.upkey:
-                this.upSpeed = 0;
-                break;
-            case this.downkey:
-                this.downSpeed = 0;
-                break;
-            case this.rightkey:
-                this.rightSpeed = 0;
-                break;
-            case this.leftkey:
-                this.leftSpeed = 0;
-                break;
-        }
-    }
-    update() {
-        let newY = this.y - this.upSpeed + this.downSpeed;
-        if (newY > 0 && newY + 100 < window.innerHeight)
-            this.y = newY;
-        let newX = this.x - this.leftSpeed + this.rightSpeed;
-        if (newX > 0 && newX + 100 < window.innerHeight)
-            this.x = newX;
-        if (this.jumping) {
-            this.jumpy += this.gravity;
-            this.y += this.jumpy;
-        }
-        if (this.y >= window.innerHeight - this.player.clientHeight) {
-            this.jumping = false;
-            this.jumpy = 0;
-        }
-        this.player.style.transform = `translate(${this.x}px, ${this.y}px)`;
-    }
-    update2() {
-        this.downSpeed = 0;
-=======
     constructor() {
         this.worms = [];
         this.platform = document.createElement("platform");
@@ -247,7 +137,6 @@ class Worm {
     }
     die() {
         this.worm.remove();
->>>>>>> db80a2449a2bea7753a3f0c518edf4538537daa5
     }
 }
 //# sourceMappingURL=main.js.map
