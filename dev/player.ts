8a0807943800b513b1e85f1764686aa8b029ca2c
Robin Michael van Doorn <robinmvdoorn@gmail.com>
2020-06-14 12:39:27 +0200

    Add getters and setters.

--- dev/player.ts
+++ dev/player.ts
@@ -1,9 +1,9 @@
-
 class Player {
     private player : HTMLElement
-    
-    private x: number
-    private y: number 
+    private div : HTMLElement
+
+    private _x: number = 0
+    private _y: number = 0
 
     private downkey: number = 0
     private upkey: number = 0
@@ -14,6 +14,19 @@ class Player {
     private upSpeed: number = 0
     private rightSpeed: number = 0
     private leftSpeed: number = 0 
+
+    public get x(): number {
+        return this._x
+    }
+    public set x(value: number) {
+        this._x = value
+    }
+    public get y(): number {
+        return this._y
+    }
+    public set y(value: number) {
+        this._y = value
+    }
 
     constructor(){
         //create player element
@@ -28,8 +41,8 @@ class Player {
         this.leftkey = 37
 
         //temporary placement
-        this.x = 200
-        this.y = 200
+        this._x = 200
+        this._y = 200
 
         //key events
         window.addEventListener("keydown", (e: KeyboardEvent) => this.onKeyDown(e))
@@ -80,13 +93,13 @@ class Player {
 
     //movement of character
     public move(){
-        let newY = this.y - this.upSpeed + this.downSpeed
-        if (newY > 0 && newY < window.innerHeight) this.y = newY
+        let newY = this._y - this.upSpeed + this.downSpeed
+        if (newY > 0 && newY < window.innerHeight) this._y = newY
 
-        let newX = this.x - this.leftSpeed + this.rightSpeed
-        if (newX > 0 && newX < window.innerWidth) this.x = newX
+        let newX = this._x - this.leftSpeed + this.rightSpeed
+        if (newX > 0 && newX < window.innerWidth) this._x = newX
 
-        this.player.style.transform = `translate(${this.x}px, ${this.y}px)`
+        this.player.style.transform = `translate(${this._x}px, ${this._y}px)`
     }
 
     //stop the downwards movement 
