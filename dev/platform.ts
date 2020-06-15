72058e3778615b0afd1071281a4e587ab194cfb1
Robin Michael van Doorn <robinmvdoorn@gmail.com>
2020-06-15 11:23:28 +0200

    Add random amount of worms to platform.

--- dev/platform.ts
+++ dev/platform.ts
@@ -5,11 +5,26 @@ class Platform{
     private x : number = 0
     private y : number = 0
 
+    private speed : number = 0
+
+    private worms : Worm[] = []
+
+
+
     constructor(){
 
         this.element = document.createElement("platform"); 
         let game = document.getElementsByTagName("game")[0];
         game.appendChild(this.element);
+
+        this.speed = Math.random() * 3 + 1
+        this.x = this.x
+        this.y = this.y
+
+        for (let i = 0; i < Math.random() * 4; i++) {
+            this.worms.push(new Worm(i * 100 + 20, -60))
+            
+        }
         
     }
 
