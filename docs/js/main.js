c411e7a7b5da07dd4bf306f782cb6dad82d74358
Robin Michael van Doorn <robinmvdoorn@gmail.com>
2020-06-15 12:33:22 +0200

    Watch.

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,8 +1,29 @@
+class Platform {
+    constructor() {
+        this.x = 0;
+        this.y = 0;
+        this.speed = 0;
+        this.worms = [];
+        this.element = document.createElement("platform");
+        let game = document.getElementsByTagName("game")[0];
+        game.appendChild(this.element);
+        this.speed = Math.random() * 3 + 1;
+        this.x = this.x;
+        this.y = this.y;
+        for (let i = 0; i < Math.random() * 4; i++) {
+            this.worms.push(new Worm(i * 100 + 20, -60));
+        }
+    }
+    getPlatformRectangle() {
+        return this.element.getBoundingClientRect();
+    }
+}
 class Game {
     constructor() {
         this.worm = [];
-        this.platform = new Platform();
+        this.spit = [];
         this.player = new Player();
+        this.platform = new Platform();
         this.gameLoop();
     }
     gameLoop() {
@@ -20,7 +41,19 @@ class Game {
             this.player.stopMove();
         }
         else {
-            this.player.update();
+            this.player.updateSpeed();
+        }
+        for (const worm of this.worm) {
+            if (this.checkCollision(worm.getWormRectangle(), this.player.getPlayerRectangle())) {
+                worm.die();
+                console.log("worm");
+            }
+        }
+        for (const spit of this.spit) {
+            spit.move();
+            if (this.checkCollision(spit.getSpitRectangle(), this.player.getPlayerRectangle())) {
+                this.player.die();
+            }
         }
         requestAnimationFrame(() => this.gameLoop());
     }
@@ -32,22 +65,11 @@ class Game {
     }
 }
 window.addEventListener("load", () => new Game());
-class Platform {
-    constructor() {
-        this.x = 0;
-        this.y = 0;
-        this.element = document.createElement("platform");
-        let game = document.getElementsByTagName("game")[0];
-        game.appendChild(this.element);
-    }
-    getPlatformRectangle() {
-        return this.element.getBoundingClientRect();
-    }
-}
 class Player {
     constructor() {
         this.x = 0;
         this.y = 0;
+        this.jumpy = 0;
         this.speed = 2;
         this.downSpeed = 0;
         this.upSpeed = 0;
@@ -57,6 +79,9 @@ class Player {
         this.upkey = 0;
         this.rightkey = 0;
         this.leftkey = 0;
+        this.jumpHeight = -10;
+        this.jumping = false;
+        this.jumpGravity = 0.2;
         this.element = document.createElement("player");
         let game = document.getElementsByTagName("game")[0];
         game.appendChild(this.element);
@@ -67,12 +92,8 @@ class Player {
         window.addEventListener("keydown", (e) => this.onKeyDown(e));
         window.addEventListener("keyup", (e) => this.onKeyUp(e));
     }
-    getSpeed() {
-        return this.speed;
-    }
-    setSpeed(speed) {
-        this.speed = speed;
-    }
+    getSpeed() { return this.speed; }
+    setSpeed(speed) { this.speed = speed; }
     gravity() {
         this.y += this.speed;
         this.element.style.transform = `translate(${this.x}px, ${this.y}px)`;
@@ -88,16 +109,19 @@ class Player {
     onKeyDown(e) {
         switch (e.keyCode) {
             case this.upkey:
-                this.upSpeed = 3.5;
-                break;
+                if (!this.jumping) {
+                    this.jumping = true;
+                    console.log("jumped");
+                    this.jumpy = this.jumpHeight;
+                }
             case this.downkey:
-                this.downSpeed = 3.5;
+                this.downSpeed = 3;
                 break;
             case this.rightkey:
-                this.rightSpeed = 3.5;
+                this.rightSpeed = 3;
                 break;
             case this.leftkey:
-                this.leftSpeed = 3.5;
+                this.leftSpeed = 3;
                 break;
         }
     }
@@ -124,13 +148,40 @@ class Player {
         let newX = this.x - this.leftSpeed + this.rightSpeed;
         if (newX > 0 && newX < window.innerWidth)
             this.x = newX;
+        if (this.jumping) {
+            this.jumpy += this.jumpGravity;
+            this.y += this.jumpy;
+        }
         this.element.style.transform = `translate(${this.x}px, ${this.y}px)`;
     }
     stopMove() {
         this.downSpeed = 0;
     }
-    update() {
+    updateSpeed() {
         this.x += this.speed;
+    }
+    die() {
+        this.element.remove();
+    }
+}
+class Spit {
+    constructor(x, y) {
+        this.spit = document.createElement("spit");
+        let worm = document.getElementsByTagName("worm")[0];
+        worm.appendChild(this.spit);
+        this.x = x;
+        this.y = y;
+        this.xspeed = Math.floor(Math.random() * 10) || Math.floor(Math.random() * -10);
+        this.yspeed = Math.floor(Math.random() * 10) || Math.floor(Math.random() * -10);
+        this.move();
+    }
+    getSpitRectangle() {
+        return this.spit.getBoundingClientRect();
+    }
+    move() {
+        this.x += this.xspeed;
+        this.y += this.yspeed;
+        this.spit.style.transform = `translate(${this.x}px, ${this.y}px)`;
     }
 }
 class Worm {
@@ -142,7 +193,7 @@ class Worm {
         this.y = y;
         this.worm.style.transform = `translate(${this.x}px, ${this.y}px)`;
     }
-    getRectangle() {
+    getWormRectangle() {
         return this.worm.getBoundingClientRect();
     }
     die() {
