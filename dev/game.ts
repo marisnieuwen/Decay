afd8520c378f7bb3c161adba186e486f2b4ce2df
Robin Michael van Doorn <robinmvdoorn@gmail.com>
2020-06-17 15:59:59 +0200

    Add check for left & right collision on platform.

--- dev/game.ts
+++ dev/game.ts
@@ -8,7 +8,7 @@ class Game {
     private player: Player;
     //private worms: Worm[] = []
     private spit: Spit[] = [] //werk nog niet?
-    private ground: number = 500
+    private ground: number = 700
 
     constructor() {
         this.player = new Player();
@@ -51,8 +51,12 @@ class Game {
     // check player platform bottom collision only
     // probleem : je moet nog wel checken of je het platform links of rechts raakt!!
     private checkBottomCollision(platform: ClientRect, player: ClientRect) { //check collision
-        return (platform.top <= player.bottom && player.top <= platform.bottom)
+        return (platform.top <= player.bottom &&
+                player.top <= platform.bottom &&
+                platform.left <= player.right &&
+                player.left <= platform.right)
     }
+
     // check worm en spit collision
     private checkCollision(a: ClientRect, b: ClientRect) { //check collision
         return (a.left <= b.right &&
